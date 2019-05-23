7986dbbb6144e17749eee392f6b9e0e8dc6e0f03
gun1x <gheorghe@linux.com>
2019-05-23 15:04:09 +0300

    small fixes

--- start.sh
+++ start.sh
@@ -2,10 +2,7 @@
 
 # handle wireguard stuff required for the script
 ip link add dev "${WIREGUARD_INTERFACE}" type wireguard
-touch private-key
-chmod 600 private-key
-wg genkey > private-key
-wg set "${WIREGUARD_INTERFACE}" listen-port 1337 private-key private-key
+wg set "${WIREGUARD_INTERFACE}" listen-port 1337 private-key <(wg genkey)
 ip link set up dev "${WIREGUARD_INTERFACE}"
 
 # generate TLS key and cert
