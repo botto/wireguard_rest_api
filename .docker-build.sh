cabbfbda9fed89d445e3fb658c23c460b8450247
gun1x <gheorghe@linux.com>
2019-05-25 12:48:17 +0300

    also added docker.io repository

--- .docker-build.sh
+++ .docker-build.sh
@@ -4,4 +4,5 @@
 CGO_ENABLED=0 go build
 docker build -t registry.gitlab.com/gun1x/wireguard_rest_api .
 docker push registry.gitlab.com/gun1x/wireguard_rest_api
-
+docker tag registry.gitlab.com/gun1x/wireguard_rest_api gunix/wireguard_rest_api:latest
+docker push gunix/wireguard_rest_api:latest
